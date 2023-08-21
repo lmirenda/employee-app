32888d2036f83d1e54fae1b2484c35d14adefbf4
lmirenda <lucamateomirenda@gmail.com>
2023-08-21 16:30:16 -0300

    Create employee model manager

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "employees"
+    "employees",
 ]
 
 MIDDLEWARE = [
@@ -56,8 +56,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- employees/models/employee.py
+++ employees/models/employee.py
@@ -1,6 +1,34 @@
-import datetime
+from datetime import datetime, timedelta
 
 from django.db import models
+from django.db.models import Q
+
+
+class EmployeeManager(models.Manager):
+    def filter_by_date_range(self, field_name, start_date, days):
+        start_date_obj = datetime.strptime(start_date, "%d-%m-%Y")
+
+        end_date_obj = start_date_obj + timedelta(days=days)
+
+        if start_date_obj.month == end_date_obj.month:
+            date_range_filter = (
+                Q(**{f"{field_name}__month": start_date_obj.month, f"{field_name}__day__gte": start_date_obj.day,
+                     f"{field_name}__day__lte": end_date_obj.day})
+            )
+        else:
+            date_range_filter = (
+                Q(**{f"{field_name}__month": start_date_obj.month, f"{field_name}__day__gte": start_date_obj.day}) |
+                Q(**{f"{field_name}__month": end_date_obj.month, f"{field_name}__day__lte": end_date_obj.day}) |
+                Q(**{f"{field_name}__month__gt": start_date_obj.month, f"{field_name}__month__lt": end_date_obj.month})
+            )
+
+        return date_range_filter
+
+    def birthdays_within_date_range(self, start_date, days):
+        return self.get_queryset().filter(self.filter_by_date_range("birthday", start_date, days))
+
+    def enrollments_within_date_range(self, start_date, days):
+        return self.get_queryset().filter(self.filter_by_date_range("enrollment_date", start_date, days))
 
 
 class Employee(models.Model):
@@ -12,8 +40,4 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
-    def has_birthday_in_range(self, start_date, days):
-        return start_date <= self.birthday <= start_date + datetime.timedelta(days=days)
-
-    def has_enrollment_in_range(self, start_date, days):
-        return start_date <= self.enrollment_date <= start_date + datetime.timedelta(days=days)
+    objects = EmployeeManager()
