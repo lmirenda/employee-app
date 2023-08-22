8560bbc11a79afea71e9bd9de7ae512f564a3e45
lmirenda <lucamateomirenda@gmail.com>
2023-08-22 09:48:31 -0300

    add permissions

--- employees/views/employee_views.py
+++ employees/views/employee_views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
 
 from employees.models import Employee
+from employees.permissions import UserOrReadOnly
 from employees.serializers.employee_serializer import EmployeeSerializer
 
 
 class EmployeeList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [UserOrReadOnly]
 
 
 class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
