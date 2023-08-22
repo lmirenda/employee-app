2a3199ec2122478d25eb511e423b8645fecdf8a5
lmirenda <lucamateomirenda@gmail.com>
2023-08-21 21:21:37 -0300

    Fix formatting

--- employees/urls.py
+++ employees/urls.py
@@ -5,8 +5,8 @@ from employees.views.birthday_view import BirthdayFilterListView
 from employees.views.employee_views import EmployeeList, EmployeeDetail
 
 urlpatterns = [
-    path("/", EmployeeList.as_view()),
-    path("/<int:pk>/", EmployeeDetail.as_view()),
-    path("/birthday/", BirthdayFilterListView.as_view()),
-    path("/anniversary/", AnniversaryFilterListView.as_view()),
+    path("", EmployeeList.as_view()),
+    path("<int:pk>/", EmployeeDetail.as_view()),
+    path("birthday/", BirthdayFilterListView.as_view()),
+    path("anniversary/", AnniversaryFilterListView.as_view()),
 ]
--- employees/views/birthday_view.py
+++ employees/views/birthday_view.py
@@ -11,21 +11,16 @@ class BirthdayFilterListView(ListAPIView):
     serializer_class = EmployeeSerializer
 
     def get_queryset(self):
-        # Get start_date and days from query parameters
         start_date = self.request.query_params.get(
             "start_date", datetime.now().strftime("%Y-%m-%d")
         )
         days = int(self.request.query_params.get("days", 0))
-        # Call the custom manager method to get the filtered queryset
-        queryset = Employee.objects.birthdays_within_date_range(start_date, days)
-
-        return queryset
+        return Employee.objects.birthdays_within_date_range(start_date, days)
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
 
-        # Manipulate serialized data to include the "event_type" field
         modified_data = []
         for item in serializer.data:
             item["event_type"] = "birthday"
